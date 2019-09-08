46a078b7db082f8d5d72b982f60ec50719481424
Alex Nakorn <alexnakorn@gmail.com>
2019-09-07 21:44:07 -0700

    Renaming services to models

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles/App.scss';
 import Header from './components/Header';
 import UpcomingMovie from './data/upcoming.json';
-import PollsDebug from './services/polls-debug';
+import PollsDebug from './models/polls-debug';
 
 const App: React.FC = () => {
     return (
