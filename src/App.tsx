e9838c25fa3acfa89dc5cbf8542f95734f29bd51
Alex Nakorn <alexnakorn@gmail.com>
2019-09-07 17:16:18 -0700

    Adding polls service and basic functionality for creating polls and voting

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './styles/App.scss';
 import Header from './components/Header';
+import PollsDebug from './services/polls-debug';
 
 const App: React.FC = () => {
     return (
         <div className="App">
-            <Header/>
+            <PollsDebug />
+            <Header />
         </div>
-  );
+    );
 }
 
 export default App;
